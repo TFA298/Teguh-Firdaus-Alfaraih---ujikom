451dde14cec6481bc87b501cafec1d2f7838c1ef
Teguh Firdaus Alfaraih <hyperionfubuki@gmail.com>
2024-06-19 09:48:48 +0700

    Add Scene Main Menu

---
+++ Assets/Script/Main_Menu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Main_Menu : MonoBehaviour
+{
+    public GameObject play_Button;
+    public GameObject exit_Button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Play_Button(){
+        
+    }
+
+    void Exit_Button(){
+        EditorApplication.Exit(0);
+    }
+}
